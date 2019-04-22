78ffd03be832d91b8f0d809b0ece6d7c8c610c2e
alexleru <alexleru@yandex.ru>
2019-04-22 12:01:25 +0300

    lesson6

--- src/main/java/ru/alexey/weather/Fragments/WeatherAdapter.java
+++ src/main/java/ru/alexey/weather/Fragments/WeatherAdapter.java
@@ -2,7 +2,6 @@ package ru.alexey.weather.Fragments;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
